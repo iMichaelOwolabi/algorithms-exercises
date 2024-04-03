9d0bf5132218c4956da23e85e220932f22af6fb7
Michael Owolabi <michael.i.owolabi@gmail.com>
2024-04-03 14:51:53 +0100

    solve bubble sort challenge

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,12 +11,27 @@
 */
 
 function bubbleSort(nums) {
-  // code goes here
+  // create a temporary variable to hold values in to be replaced
+  // Loop through the array and compare if the current element is greater than the next element, if yes, swap the elements position in the array by doing the following
+  // Set the bigger element into the temp variable then set the smaller into the position of the smaller one and vice versa from the temp.
+  
+  let temp;
+  for (i = 0; i < nums.length; i++) {
+    for (j = 0; j < nums.length; j++) {
+      if (nums[j] > nums[j + 1]) {
+        temp = nums[j];
+        nums[j] = nums[j+1]
+        nums[j+1] = temp;
+      }
+    }
+  }
+
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
