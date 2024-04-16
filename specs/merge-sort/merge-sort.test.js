939c77075135f2406cfac6375ceb2ef2bfc74022
Michael Owolabi <michael.i.owolabi@gmail.com>
2024-04-16 16:23:31 +0100

    solve merge sort challenge

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -8,11 +8,31 @@
 
 const mergeSort = (nums) => {
   // code goes here
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  return merge(mergeSort(nums.splice(0, nums.length / 2)), mergeSort(nums));
 };
+
+const merge = (firstArray, secondArray) => {
+  let finalArray = [];
+
+  while (firstArray.length && secondArray.length) {
+    if (firstArray[0] <= secondArray[0]) {
+      finalArray.push(firstArray.shift());
+    }
+    else {
+      finalArray.push(secondArray.shift())
+    }
+  }
+
+  return finalArray.concat(firstArray, secondArray);
+}
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
